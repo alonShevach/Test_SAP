9d495bf236e8d77ce69fcbbfc6092a304df857bf
Alon Shevach <67096125+alonShevach@users.noreply.github.com>
2021-04-28 17:00:37 +0300

    Create Test.sh

---
+++ Test.sh
@@ -0,0 +1,7 @@
+echo CPU: `top -b -n1 | grep "Cpu(s)" | awk \'{print $2 + $4}\'` 
+            FREE_DATA=`free -m | grep Mem` 
+            CURRENT=`echo $FREE_DATA | cut -f3 -d\' \'`
+            TOTAL=`echo $FREE_DATA | cut -f2 -d\' \'`'''
+echo free -t
+echo uname -a
+echo cat /proc/version
